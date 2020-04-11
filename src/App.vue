b95fe880348cee93ee2a7fd75985d312aa4eba5b
Mohammad Rahhal <m.r992@hotmail.com>
2020-04-11 13:54:14 +0300

    Prepare

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    Hello World!
+    <svg-github></svg-github>
+    <a href="github.com/mrahhal">github</a>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script lang="ts" src="./App.ts"></script>
+<style lang="scss" src="./App.scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import '@/components/globals';
+
+import Vue from 'vue';
+
+import App from './App.vue';
 
 Vue.config.productionTip = false
 
