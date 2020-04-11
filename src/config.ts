1eb5189f1e029e3f5a201b2abae9c0ca226a4b52
Mohammad Rahhal <m.r992@hotmail.com>
2020-04-11 15:32:45 +0300

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  name: 'Mohammad Rahhal',
+  title: 'Software Engineer',
+  email: 'm.r992@hotmail.com',
+  website: 'http://mrahhal.net',
+  githubUsername: 'mrahhal',
+  to: 'MRH',
+  position: 'Software Engineer on the .NET team'
+};
