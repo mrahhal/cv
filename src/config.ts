7cf2e4fe0fb6fdcec16f97bd9f9948b6c238551c
Mohammad Rahhal <m.r992@hotmail.com>
2020-04-13 17:11:17 +0300

    Convert config to be json based

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,1 @@
-export const config = {
-  name: 'Mohammad Rahhal',
-  title: 'Software Engineer',
-  email: 'm.r992@hotmail.com',
-  website: 'http://mrahhal.net',
-  githubUsername: 'mrahhal',
-  to: 'MRH',
-  position: 'Software Engineer on the .NET team',
-};
+export { default as config } from './content/config.json';
